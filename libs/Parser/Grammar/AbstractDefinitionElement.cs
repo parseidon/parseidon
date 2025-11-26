974ab225f2a9e0c4de2cd8419538382e0caad499
Michael T <michael@mtxmail.de>
2025-11-26 15:18:11 +0100

    Reverted changes made in wrong grammar

--- libs/Parser/Grammar/AbstractDefinitionElement.cs
+++ libs/Parser/Grammar/AbstractDefinitionElement.cs
@@ -3,12 +3,4 @@ namespace Parseidon.Parser.Grammar;
 public class AbstractDefinitionElement : AbstractGrammarElement
 {
     public AbstractDefinitionElement(MessageContext messageContext, ASTNode node) : base(messageContext, node) { }
-
-    private sealed record Pattern
-    {
-        public String match = String.Empty;
-    }
-
-    public virtual Object GetPattern() => new Pattern() { match = "" };
-
 }
