e4d20d8c29e7552cdba3ee62b7a58326a5705a63
Michael T <michael@mtxmail.de>
2025-12-11 08:13:31 +0100

    Removed useless code

--- libs/Parser/Grammar/Operators/UseRuleNameAsErrorMarker.cs
+++ libs/Parser/Grammar/Operators/UseRuleNameAsErrorMarker.cs
@@ -9,7 +9,7 @@ public class UseDefinitionNameAsErrorMarker : AbstractMarker
     public override String ToParserCode(Grammar grammar)
     {
         Definition definition = GetDefinition();
-        String errorName = definition.KeyValuePairs.TryGetValue(Grammar.GrammarPropertyErrorName, out String temp) ? $"\"{temp}\"" : $"\"{definition.Name}\"" ?? "errorName";
+        String errorName = definition.KeyValuePairs.TryGetValue(Grammar.GrammarPropertyErrorName, out String temp) ? $"\"{temp}\"" : $"\"{definition.Name}\"";
         String result = "";
         result += $"SetErrorName(actualNode, state, {errorName},\n";
         result += Indent($"(actualNode, errorName) => {Element?.ToParserCode(grammar)}") + "\n";
